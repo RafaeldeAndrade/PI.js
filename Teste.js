ddc3b63ae1f562b3bc6140ac6717336741e47ee3
RafaeldeAndrade <163475012+RafaeldeAndrade@users.noreply.github.com>
2024-03-23 20:21:15 -0300

    Create Teste.js

---
+++ Teste.js
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<head>
+    <html lang="pt-br">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    
+    <title>Barbearia Beleza Pura</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"> <!--Font  txt-->
+</head>
+
+<body>
+    <header>
+        <div id="box">
+            <nav>
+                <h1><img src="img/logo.jpg" alt="logo da Barbearia Beleza Pura"></h1>
+
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="product.html">Produtos</a></li>
+                    <li><a href="contact.html">Contantato</a></li>
+                </ul>
+            </nav>    
+        </div>
+    </header>
+    <main>
+        <img src="img/on.jpg", id="banner" alt="header with itens of barber">
+
+        <section class="main">
+            <h2 class="title_main"> Barbearia Beleza Pura </h2>
+
+            <img src="img/cut.jpg" id="cut" alt="cuting barber of men">
+
+            <p id="greate"> A localização da barbearia, <strong> Beleza Pura </strong> é no bairro Jardim Iracema, rua Ezair de Pacheco Meneses, trabalhamos de segunda a sexta-feira das 8:00 as 17:00 horas </p>
+
+            <p> Nossa missão é: <em> <strong> "Fazer um trabalho Personalizado para que os nossos clientes se sintam satisfeitos". </strong> </em> </p>
+
+            
+        </section>
+
+        <section class="local">
+            <h2>Local</h2>
+            <p>Nosso estabelecimento esta localizado em Monteiro Lobato SP</p>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.6714350869615!2d-45.838565124688074!3d-22.962324579216006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc410027342289%3A0x2beda0fd19732554!2sBarbearia%20Beleza%20Pura!5e0!3m2!1spt-BR!2sbr!4v1710955442830!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </section>
+        <div class="benefits-video">
+            <section class="benefits">
+                <h2 class="title_main"> Beneficios </h2>
+                <div>
+                    <img src="img/4.png" id=four alt="number four">
+                    <ul>
+                        <li>Corte Personalizado</li>
+                        <li>Barba Personalizada</li>
+                        <li>Atendimento Profissional</li>
+                        <li>Atendimento Diferenciado</li>
+                    </ul>
+                </div>    
+            </section>
+
+            <div class="video">
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/z1SC4RYyxOc" frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <img src="img/below.jpg", id="banner_below" alt="cuting hair">
+    </main>
+    <footer>
+        <img src="img/logo.png" alt="logo da Barbearia Beleza Pura">
+        <p class="copyright">&copy;Copyright: Projeto do Grupo de PI. 2024</p>on of (unicode)-->
+    </footer>
+
+</body>
+</html>
